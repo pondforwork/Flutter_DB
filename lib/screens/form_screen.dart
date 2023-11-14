9083e125289d31501c45030a22201b931f56208d
pondforwork <pharadonsiriforwork@gmail.com>
2023-11-14 10:33:36 +0700

    Set Keyboard Type

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -9,18 +9,22 @@ class FormScreen extends StatelessWidget {
         appBar: AppBar(
           title: const Text("หน้าที่ 2"),
         ),
-        body: Form(
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: const InputDecoration(labelText: "ชื่อรายการ"),
-              ),
-              TextFormField(
-                decoration: const InputDecoration(labelText: "ใส่จำนวนเงิน"),
-              ),
-              TextButton(onPressed: () {  },
-              child: const Text("Add"))
-            ],
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(labelText: "ชื่อรายการ"),
+                ),
+                TextFormField(
+                    decoration:
+                        const InputDecoration(labelText: "ใส่จำนวนเงิน"),
+                    keyboardType: TextInputType.number),
+                TextButton(onPressed: () {}, child: const Text("Add"))
+              ],
+            ),
           ),
         ) // This trailing comma makes auto-formatting nicer for build methods.
 
