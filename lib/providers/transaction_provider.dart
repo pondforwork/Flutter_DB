443c84b30f775f2f05485af51b9f14c23a9fcad2
pondzaa114 <pharadonsiriforwork@gmail.com>
2023-11-17 10:17:27 +0700

    Prepare Provider Data

---
+++ lib/providers/transaction_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_database/models/Transaction.dart';
+
+class TransactionPrivider with ChangeNotifier {
+  List<Transaction> transactions = [
+    Transaction(title: "หนังสือ", amount: 500, date: DateTime.now()),
+    Transaction(title: "รองเท้า", amount: 200, date: DateTime.now())
+  ];
+
+  List<Transaction>getTransaction(){
+    return transactions;
+  }
+
+  addTranscation(Transaction statement){
+    return transactions.add(statement);
+  }
+}
