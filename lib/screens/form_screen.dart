91a1108854cc70fb34e223c1a164b3e94aa69f1f
pondforwork <pharadonsiriforwork@gmail.com>
2023-11-20 11:25:39 +0700

    Add One Validate

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -1,46 +1,55 @@
 import 'package:flutter/material.dart';
 
 class FormScreen extends StatelessWidget {
-  const FormScreen({super.key});
+  const FormScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final TextEditingController transactionName = TextEditingController();
     final TextEditingController transactionAmount = TextEditingController();
-
+    final formKey = GlobalKey<FormState>();
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("หน้าที่ 2"),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Form(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(labelText: "ชื่อรายการ"),
-                  controller: transactionName,
-                ),
-                TextFormField(
-                    decoration:
-                        const InputDecoration(labelText: "ใส่จำนวนเงิน"),
-                    controller: transactionAmount,
-                    keyboardType: TextInputType.number),
-                TextButton(
-                    onPressed: () {
-                      String name = transactionName.text;
-                      String amount = transactionName.text;
-                      print(name);
-                      print(amount);
-                      Navigator.pop(context);
-                    },
-                    child: const Text("Add"))
-              ],
-            ),
+      appBar: AppBar(
+        title: const Text("หน้าที่ 2"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                key: Key('transactionName'),
+                decoration: const InputDecoration(labelText: "ชื่อรายการ"),
+                controller: transactionName,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    print("empty");
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(labelText: "ใส่จำนวนเงิน"),
+                controller: transactionAmount,
+                keyboardType: TextInputType.number,
+              ),
+              TextButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text('Processing Data')),
+                    );
+                  }
+                },
+                child: const Text("Add"),
+              ),
+            ],
           ),
-        ) // This trailing comma makes auto-formatting nicer for build methods.
-
-        );
+        ),
+      ),
+    );
   }
 }
